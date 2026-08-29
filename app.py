18412e04419456242830cc80816c711ba2815eb1
alexeyglinkin <aleksey.glinkin@skillbox.ru>
2026-08-29 23:27:44 +0300

    new design2

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+from pathlib import Path
 from flask import Flask, send_file
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = Flask(
     __name__,
-    static_folder="assets",
+    static_folder=str(BASE_DIR / "assets"),
     static_url_path="/assets"
 )
 
 
 @app.route("/")
 def index():
-    return send_file("index.html")
+    return send_file(BASE_DIR / "index.html")
 
 
 if __name__ == "__main__":
